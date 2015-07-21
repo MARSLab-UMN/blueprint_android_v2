b0be9b5585167f4fb351dbee49336baefae04a74
John O'Leary <olear121@umn.edu>
2015-07-21 13:58:53 -0500

    Can navigate forward through folders and chose bluepritns or trajectories.

--- BlueprintAndroidApp/app/src/main/java/edu/umn/mars/blueprintandroidapp/MainActivity.java
+++ BlueprintAndroidApp/app/src/main/java/edu/umn/mars/blueprintandroidapp/MainActivity.java
@@ -18,17 +18,25 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
     //In an Activity
     static final private String LOG_TAG = "BlueprintAndroidApp";
-    private String[] mFileList;
+    private ArrayList<String> mFileList = new ArrayList<String>();;
     private String mChosenFile;
+    private String mCurrentDir;
+    private LoadType mLoadType;
     private static final String FTYPE = ".txt";
     private static final String PNGTYPE = ".png";
     private static final String JPGTYPE = ".jpg";
     private List<Float> traj_vertices;
+
+    public enum LoadType {
+        BLUEPRINT, TRAJECTORY
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,84 +85,10 @@ public class MainActivity extends ActionBarActivity {
         toast.show();
     }
 
-    private void loadFileList(String baseFolderPath) {
-        File mPath = new File(baseFolderPath);
-
-        try {
-            mPath.mkdirs();
-        }
-        catch(SecurityException e) {
-            Log.e(LOG_TAG, "unable to write on the sd card " + e.toString());
-        }
-        if(mPath.exists()) {
-            FilenameFilter filter = new FilenameFilter() {
-
-                @Override
-                public boolean accept(File dir, String filename) {
-                    File sel = new File(dir, filename);
-//                    return filename.contains(FTYPE) || sel.isDirectory();
-                    return true;
-                }
-
-            };
-            mFileList = mPath.list(filter);
-        }
-        else {
-            mFileList= new String[0];
-        }
-    }
-
-    private boolean FileIsImage() {
-        return mChosenFile.contains(PNGTYPE) || mChosenFile.contains(JPGTYPE);
-    }
-
-    private boolean FileIsData() {
-        return true;
-    }
-
-    protected Dialog createFileSelectorDialog() {
-        loadFileList(Environment.getExternalStorageDirectory() + "/");
-        Dialog dialog = null;
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setTitle("Choose your file");
-        if (mFileList == null) {
-            Log.e(LOG_TAG, "Showing file picker before loading the file list");
-            dialog = builder.create();
-            return dialog;
-        }
-        builder.setItems(mFileList, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                mChosenFile = mFileList[which];
-                Log.i(LOG_TAG, "Selected: " + mChosenFile);
-
-                if (FileIsImage()) {
-                    Log.i(LOG_TAG, "You have selected an image.");
-                } else if (FileIsData()) {
-                    Log.i(LOG_TAG, "You have selected a data file.");
-                }
-            }
-        });
-
-        dialog = builder.show();
-        return dialog;
-    }
-
-    public void LoadAlignment() {
-        PrintNotYetImplemented("LoadAlignment");
-    }
-
-    public void SaveAlignment() {
-        PrintNotYetImplemented("SaveAlignment");
-    }
-
-    public void SelectTrajectory(View view) {
-        Dialog dialog = createFileSelectorDialog();
-        dialog.show();
-
+    private void readTrajData() {
         // write on SD card file data in the text box
         try {
-            File myFile = new File("/sdcard/" + mChosenFile);
+            File myFile = new File(mCurrentDir + mChosenFile);
             FileInputStream fIn = new FileInputStream(myFile);
             BufferedReader myReader = new BufferedReader(
                     new InputStreamReader(fIn));
@@ -176,7 +110,125 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    private void readImageData() {
+        PrintNotYetImplemented("readImageData");
+    }
+
+    private void loadFileList(String baseFolderPath) {
+        File mPath = new File(baseFolderPath);
+
+        try {
+            mPath.mkdirs();
+        } catch (SecurityException e) {
+            Log.e(LOG_TAG, "unable to write on the sd card " + e.toString());
+        }
+        if (mPath.exists()) {
+            FilenameFilter dirFilter = new FilenameFilter() {
+
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    return sel.isDirectory();
+                }
+
+            };
+            FilenameFilter fileFilter = new FilenameFilter() {
+
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    return !sel.isDirectory();
+                }
+
+            };
+            List<String> dirList = Arrays.asList(mPath.list(dirFilter));
+            mFileList.addAll(dirList);
+            List<String> fileList = Arrays.asList(mPath.list(fileFilter));
+            mFileList.addAll(fileList);
+        } else {
+            mFileList.clear();
+        }
+    }
+
+    private boolean FileIsImage() {
+        return mChosenFile.contains(PNGTYPE) || mChosenFile.contains(JPGTYPE);
+    }
+
+    private boolean FileIsDir() {
+        File sel = new File(mCurrentDir, mChosenFile);
+        return sel.isDirectory();
+    }
+
+    private boolean FileIsData() {
+        return !FileIsImage() && !FileIsDir();
+    }
+
+    protected Dialog createFileSelectorDialog(String baseFolderPath, LoadType loadType) {
+        mCurrentDir = baseFolderPath;
+        mLoadType = loadType;
+        mFileList.clear();
+        loadFileList(mCurrentDir);
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Choose your file");
+        if (mFileList.isEmpty()) {
+            Log.e(LOG_TAG, "Showing file picker before loading the file list");
+            dialog = builder.create();
+            return dialog;
+        }
+        builder.setItems(mFileList.toArray(new String[mFileList.size()]),
+                        new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                mChosenFile = mFileList.get((which));
+                Log.i(LOG_TAG, "Selected: " + mChosenFile);
+
+                if (FileIsImage() && mLoadType == LoadType.BLUEPRINT) {
+                    Log.i(LOG_TAG, "You have selected an image.");
+                    readImageData();
+                } else if (FileIsData() && mLoadType == LoadType.TRAJECTORY) {
+                    Log.i(LOG_TAG, "You have selected a data file.");
+                    readTrajData();
+                } else if (FileIsDir()) {
+                    Log.i(LOG_TAG, "Selected a directory");
+                    dialog.dismiss();
+                    createFileSelectorDialog(mCurrentDir + mChosenFile + "/", mLoadType);
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Invalid file selection. Select another.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    createFileSelectorDialog(mCurrentDir, mLoadType);
+                }
+            }
+        });
+
+        dialog = builder.show();
+        if (mLoadType == LoadType.BLUEPRINT) {
+            dialog.setTitle("Select a blueprint file");
+        } else if (mLoadType == LoadType.TRAJECTORY) {
+            dialog.setTitle("Select a trajectory data file");
+        }
+        return dialog;
+    }
+
+    public void LoadAlignment() {
+        PrintNotYetImplemented("LoadAlignment");
+    }
+
+    public void SaveAlignment() {
+        PrintNotYetImplemented("SaveAlignment");
+    }
+
+    public void SelectTrajectory(View view) {
+        Dialog dialog = createFileSelectorDialog(Environment.getExternalStorageDirectory() + "/", LoadType.TRAJECTORY);
+        dialog.show();
+    }
+
     public void SelectBlueprint(View view) {
-        PrintNotYetImplemented("SelectBlueprint");
+        Dialog dialog = createFileSelectorDialog(Environment.getExternalStorageDirectory() + "/", LoadType.BLUEPRINT);
+        dialog.show();
     }
 }
